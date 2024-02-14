7c26e36bab26f8a862a57310ebd52389b97f8ef5
Marnoux <marnoux.manser@newzoo.com>
2024-02-14 13:27:00 +0100

    Exclude e2e

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	test: {
+		exclude: ['./e2e'],
 		coverage: { provider: 'v8' },
 		environment: 'jsdom',
 		globals: true,
