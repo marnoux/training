7b2be594beebe16bcbdb0c50842e597c2e757592
Marnoux <marnoux.manser@newzoo.com>
2024-02-15 08:07:28 +0100

    Add mock service worker

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,32 @@
+import { HttpResponse, http } from 'msw';
+import { Pokemon, PokemonSummary } from '../types';
+
+const createPokemonSummary = (overrides: Partial<PokemonSummary> = {}): PokemonSummary => ({
+	name: 'bulbasaur',
+	url: 'https://pokeapi.co/api/v2/pokemon/1/',
+	...overrides,
+});
+
+const createPokemonList = () => ({
+	count: 2,
+	prev: null,
+	next: null,
+	results: [
+		createPokemonSummary(),
+		createPokemonSummary({ name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' }),
+	],
+});
+
+export const createPokemon = (overrides: Partial<Pokemon> = {}): Pokemon => ({
+	id: 1,
+	name: 'bulbasaur',
+	sprites: {
+		front_default: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${overrides.id ?? 1}.png`,
+	},
+	...overrides,
+});
+
+export const handlers = [
+	http.get('*/v2/pokemon', () => HttpResponse.json(createPokemonList())),
+	http.get('*/v2/pokemon/:id', req => HttpResponse.json(createPokemon({ id: +req.params['id'] }))),
+];
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface PokemonSummary {
+	name: string;
+	url: string;
+}
+
+export interface PokemonList {
+	count: number;
+	next: string | null;
+	previous: string | null;
+	results: PokemonSummary[];
+}
+
+export interface Pokemon {
+	id: number;
+	name: string;
+	sprites: { front_default: string };
+}
